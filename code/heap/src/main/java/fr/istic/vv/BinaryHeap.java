fb7ba9787a792d0221c3ee06506fa5c0cc4a6df7
walid <82517052+waldoslastosba@users.noreply.github.com>
2024-01-29 01:28:29 +0100

    Update BinaryHeap.java

--- code/heap/src/main/java/fr/istic/vv/BinaryHeap.java
+++ code/heap/src/main/java/fr/istic/vv/BinaryHeap.java
@@ -1,17 +1,54 @@
 package fr.istic.vv;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 public class BinaryHeap<T> {
 
-    public BinaryHeap(Comparator<T> comparator) {  }
+    private Comparator<T> comparator;
+    private List<T> values;
 
-    public T pop() { return null; }
+    public BinaryHeap(Comparator<T> comparator) {
+        this.comparator = comparator;
+        this.values = new ArrayList<>();
+    }
 
-    public T peek() { return null; }
+    public T pop() {
+        if (count() == 0) throw new NoSuchElementException();
+        return values.remove(0);
+    }
 
-    public void push(T element) { }
+    public T peek() {
+        if (count() == 0) throw new NoSuchElementException();
+        return values.get(0);
+    }
 
-    public int count() { return 0; }
+    public void push(T element) {
+        if (count() == 0){
+            values.add(element);
+        } else {
+            if (comparator.compare(values.get(0), element) < 0) {
+                int idxToAdd = -1;
+                boolean searching = true;
+                for (int i = 1; i < values.size(); i++) {
+                    if (searching && comparator.compare(values.get(i), element) >= 0) { // [1, 2, 4, 6]
+                        idxToAdd = i;
+                        searching = false;
+                    }
+                }
+                if (idxToAdd > -1) {
+                    values.add(idxToAdd, element);
+                } else values.add(element);
+            } else {
+                values.add(0, element);
+            }
+        }
+    }
+
+    public int count() {
+        return values.size();
+    }
 
 }
